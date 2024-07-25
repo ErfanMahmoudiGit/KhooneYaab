e488e77ad59ecf701b5917fdd571a2e635873c49
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-07-25 12:39:41 +0330

    fixed cors bug.

--- Django/CharKhone/settings.py
+++ Django/CharKhone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'corsheaders',
 
     # local apps
     'ProximityFinder.apps.ProximityFinderConfig',
@@ -48,10 +50,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'CharKhone.urls'
@@ -126,3 +131,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True # FOR DEVELOPMENT
+
+"""
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173/"
+]
+"""
+
+CORS_ALLOWED_METHODS =  [
+    "GET",
+    "POST",
+]
