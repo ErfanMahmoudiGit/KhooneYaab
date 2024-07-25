f02dc98665c4816ac3b027af40026ce7311a726f
ftmefarrokhi <ftmeefarrokhii@gmail.com>
2024-07-25 11:54:09 +0330

    test2

--- React/src/services/createHouseService.jsx
+++ React/src/services/createHouseService.jsx
@@ -1,6 +1,17 @@
-import http from "./httpService";
-
+// import http from "./httpService";
+import axios from 'axios';
 
 export function createHouse(data){
-    return http.post('/create_house/' , data)
+    return axios.post("http://localhost:8000/ProximityFinder/api/create_house/" ,{
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            "method": 'POST',
+        },
+       
+        data
+    } )
 }
+// export function createHouse(data){
+//     return http.post('/create_house/' , data)
+// }
