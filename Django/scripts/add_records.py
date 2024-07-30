8d46d82824777fd695894c57ec99853744848405
ftmefarrokhi <ftmeefarrokhii@gmail.com>
2024-07-31 01:16:13 +0330

    6 data added

--- Django/scripts/add_records.py
+++ Django/scripts/add_records.py
@@ -16,21 +16,30 @@ df = pd.read_csv("buildings.csv")
 for _, row in df.iterrows():
     Building.objects.create(
         id=row['id'],
+        city = row['city'],
         title=row['title'],
+        category = row['category'],
         time=row['time'],
         meterage=row['meterage'],
         price=row['price'],
         price_per_meter=row['price_per_meter'],
         image=row['image'],
         description=row['description'],
-        phone=row['phone'],
+        # phone=row['phone'],
         floor=row['floor'],
         all_floors=row['all_floors'],
         #build_date=row['build_date'],
         build_date=2001,
         rooms=row['rooms'],
-        facilities=row['facilities'],
+        # facilities=row['facilities'],
         latitude=row['latitude'],
         longitude=row['longitude'],
-        priorities=row['priorities']
+        elevator = row['elevator'],
+        parking = row['parking'],
+        warehouse =row['warehouse'],
+        jahat = row['jahat'],
+        vahed = row['vahed'],
+        sanad= row['sanad'],
+        status =row['status']
+        # priorities=row['priorities']
     )
