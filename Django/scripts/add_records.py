7f3460a3fe498d1009c091e5d65f747ce4873bea
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-07-31 20:16:40 +0330

    changed columns and cleaned the data.

--- Django/scripts/add_records.py
+++ Django/scripts/add_records.py
@@ -11,35 +11,39 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CharKhone.settings')
 django.setup()
 
 from ProximityFinder.models import Building
+from extentions import utils
 
 df = pd.read_csv("buildings.csv")
 for _, row in df.iterrows():
+    mtr = utils.convert_persian_text_to_english_digits(row['meterage'])
+    prc = utils.convert_persian_text_to_english_digits(row['price'])
+    ppm = utils.convert_persian_text_to_english_digits(row['price_per_meter'])
+    vhd = utils.convert_persian_text_to_english_digits(row['vahed'])
+    rms = utils.convert_persian_text_to_english_digits(row['rooms'])
+    elevator=utils.convert_persian_text_to_english_digits(row['elevator'])
+    parking=utils.convert_persian_text_to_english_digits(row['parking'])
+    warehouse =utils.convert_persian_text_to_english_digits(row['warehouse'])
+    
     Building.objects.create(
         id=row['id'],
         city = row['city'],
-        title=row['title'],
         category = row['category'],
-        time=row['time'],
-        meterage=row['meterage'],
-        price=row['price'],
-        price_per_meter=row['price_per_meter'],
+        title=row['title'],
+        time = "2001-01-01", ### TODO
+        meterage=mtr,
+        price=prc,
+        price_per_meter=ppm,
         image=row['image'],
         description=row['description'],
-        # phone=row['phone'],
-        floor=row['floor'],
-        all_floors=row['all_floors'],
-        #build_date=row['build_date'],
-        build_date=2001,
-        rooms=row['rooms'],
-        # facilities=row['facilities'],
+        floor=vhd,#TODO
+        #all_floors=row['all_floors'], ###TODO
+        build_date=row['build_date'],
+        rooms=rms,
+        facilities = f"[{elevator},{parking},{warehouse}]", # type: ignore
+        jahat = row['jahat'],
+        document_type= row['sanad'],
+        status =row['status'],
         latitude=row['latitude'],
         longitude=row['longitude'],
-        elevator = row['elevator'],
-        parking = row['parking'],
-        warehouse =row['warehouse'],
-        jahat = row['jahat'],
-        vahed = row['vahed'],
-        sanad= row['sanad'],
-        status =row['status']
-        # priorities=row['priorities']
+        priorities = "[1,1,1]"# TODO, shows if the building is close to: hospital, school, park
     )
