9dc85c7621f988e247d8a2cc8987b48e71d86a64
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-08-16 16:02:58 +0330

    added new data for buildings and fixed the code to auto add data to the database. total building added: 978.

--- Django/scripts/add_divar_data_json.py
+++ Django/scripts/add_divar_data_json.py
@@ -13,7 +13,7 @@ django.setup()
 from ProximityFinder.models import Building
 from extentions import utils
 
-from Django.scripts.Get_Data_By_ID.divar_data import homes
+from Get_Data_By_ID.divar_data import homes
 
 for home in homes:
     mtr = utils.convert_persian_text_to_english_digits(home['meterage'])
@@ -22,30 +22,34 @@ for home in homes:
     flr = utils.convert_persian_text_to_english_digits(home['floor'])
     avhd= utils.convert_persian_text_to_english_digits(home['all_floors'])
     rms = utils.convert_persian_text_to_english_digits(home['rooms'])
-    elevator=utils.convert_persian_text_to_english_digits(home['elevator'])
-    parking=utils.convert_persian_text_to_english_digits(home['parking'])
-    warehouse =utils.convert_persian_text_to_english_digits(home['warehouse'])
+    elevator= utils.convert_persian_text_to_english_digits(home['elevator'])
+    parking= utils.convert_persian_text_to_english_digits(home['parking'])
+    warehouse = utils.convert_persian_text_to_english_digits(home['warehouse'])
+    bdt = utils.convert_persian_text_to_english_digits(home['build_date'])
     
-    Building.objects.create(
-        city=home['city'],
-        category=home['category'],
-        title=home['title'],
-        #time=home['time'],
-        time = "2001-01-01", ### TODO
-        meterage=mtr,
-        price=prc,
-        price_per_meter=ppm,
-        image=home['image'],
-        description=home['description'],
-        floor=flr,
-        all_floors=avhd,
-        build_date=home['build_date'], 
-        rooms=rms,
-        facilities = f"[{elevator},{parking},{warehouse}]", # type: ignore
-        direction=home['jahat'],
-        document_type=home['sanad'],
-        status=home['status'],
-        latitude=home['latitude'],
-        longitude=home['longitude'],
-        priorities = "[1,1,1]"# TODO, shows if the building is close to: hospital, school, park
-    )
+    if home['latitude']:
+        Building.objects.create(
+            city=home['city'],
+            category=home['category'],
+            title=home['title'],
+            #time=home['time'],
+            time = "2001-01-01", ### TODO
+            meterage=mtr,
+            price=prc,
+            price_per_meter=ppm,
+            image=home['image'],
+            description=home['description'],
+            floor=flr,
+            all_floors=avhd,
+            build_date=bdt,
+            rooms=rms,
+            facilities = f"[{elevator},{parking},{warehouse}]",
+            direction=home['jahat'],
+            document_type=home['sanad'],
+            status=home['status'],
+            latitude=home['latitude'],
+            longitude=home['longitude'],
+            priorities = "[1,1,1]"# TODO, shows if the building is close to: hospital, school, park
+        )
+    else:
+        print (str(home['id']) + " Home id does not have a latitude.")
