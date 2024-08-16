e78cfdd84f5ba0f985429df87fada85950d60732
Erifun <erfan.mahmoudi.2001@gmail.com>
2024-08-16 16:42:03 +0330

    updated urls.

--- Django/ProximityFinder/urls.py
+++ Django/ProximityFinder/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from .views import get_buildings, recommend_buildings, create_house, search_buildings, get_building_by_id, get_buildings_by_category, get_buildings_by_state
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-
 urlpatterns = [
-    path('api/buildings/', get_buildings, name='get_buildings'),
+    path('api/building/buildings/', get_buildings, name='get_buildings'),
     path('api/building/recommend_buildings/', recommend_buildings, name='recommend_buildings'),
     path('api/building/create_house/', create_house, name='create_house'),
     path('api/building/search/', search_buildings, name='search_buildings'),
